17eee29b203e29b1b3fd36c9b41bed1eab21e677
tymkiwdylan <tymkiwdylan@gmail.com>
2025-10-04 15:44:24 -0300

    aligned footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 export function Footer() {
   return (
     <footer className="border-t bg-muted/30">
-      <div className="container px-4 py-12 md:py-16">
+      <div className="container px-4 py-12 md:py-16 mx-auto max-w-7xl">
         <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
           {/* Brand */}
           <div className="lg:col-span-2">
